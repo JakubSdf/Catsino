caee87e15786377c404bf267fd7613c6772df63c
Vikous28 <74271364+Vikous28@users.noreply.github.com>
2023-10-02 18:49:30 +0200

    zakladni html bez obsahu

--- Flask-main/app/main.py
+++ Flask-main/app/main.py
@@ -7,6 +7,26 @@ app = Flask(__name__)
 def home():
     return render_template('main.html')
 
+@app.route('/ruleta')
+def ruleta():
+    return render_template('ruleta.html')
+
+
+@app.route('/crash')
+def crash():
+    return render_template('crash.html')
+
+
+@app.route('/blackjack')
+def blackjack():
+    return render_template('blackjack.html')
+
+
+@app.route('/maty')
+def maty():
+    return render_template('maty.html')
+
+
 
 #    @app.route('/', methods =["GET", "POST"])
 #    def home():
