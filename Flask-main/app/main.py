86cc6cf8b958ecf58e30ff99b588cd769521b553
Jimmy <sikulajakub2005@gmail.com>
2023-10-03 17:18:03 +0200

    updated main

--- Flask-main/app/main.py
+++ Flask-main/app/main.py
@@ -27,16 +27,6 @@ def maty():
     return render_template('maty.html')
 
 
-
-#    @app.route('/', methods =["GET", "POST"])
-#    def home():
-#         if request.method == "POST":
-#             ulice = request.form.get("Street")
- #            rows = findstreets(ulice)
- #            
- #            return render_template("results.html", rows=rows)
- #        return render_template("main.html")
-
 if __name__ == "__main__":
     app.run(port=8000)
 
