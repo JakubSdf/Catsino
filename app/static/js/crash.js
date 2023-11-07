a1ebb62702ae4831c0b5fbd46c54693df9488c6c
Jimmy <sikulajakub2005@gmail.com>
2023-11-07 18:54:10 +0100

    crash game update & delete unused images

--- app/static/js/crash.js
+++ app/static/js/crash.js
@@ -1,152 +1,172 @@
-// Get references to HTML elements
-const betAmountElement = document.getElementById('betAmount');
-const betInput = document.getElementById('betInput');
-const multiplierElement = document.getElementById('multiplier');
-const placeBetButton = document.getElementById('placeBet');
-const cashOutButton = document.getElementById('cashOut');
-const resultText = document.getElementById('result');
-const rocket = document.getElementById('rocket');
+document.addEventListener('DOMContentLoaded', function () {
+    const presetButtons = document.querySelectorAll('.preset');
+    const multiplierValue = document.getElementById('multiplier-value');
+    const betAmountInput = document.getElementById('bet-amount');
+    const placeBetButton = document.getElementById('place-bet');
+    const cashOutButton = document.getElementById('cash-out');
+    const countdown = document.getElementById('countdown');
+    const gameContent = document.getElementById('game-content');
+    const multiplierHistory = document.getElementById('multiplier-history');
 
+    let gameStarted = false;
+    let currentMultiplier = 1.00;
+    let betAmount = 0;
+    let gameInterval;
+    let countdownTime = 2;
+    let countdownInterval;
+    let speedUpInterval;
+    let betPlaced = false;
+    let historyData = []; // Array to store multiplier history
 
-// Variables for game state
-const initialRocketPosition = 0; // Adjust this value to match your CSS starting position
-let isRunning = false;
-let betAmount = 0;
-let multiplier = 1.00; // Start at 1.00
-let crashThreshold;
-let speed = 0.002; // Initial speed (adjust as needed)
-let intervalId;
+    countdownIntervalFnc();
 
-// Event listener for the "Place Bet" button
-placeBetButton.addEventListener('click', () => {
-    if (!isRunning) {
-        // Reset the game if it's not running
-        resetGame();
-        betAmount = parseFloat(betInput.value);
-
-        if (isNaN(betAmount) || betAmount <= 0) {
-            alert('Please enter a valid bet amount greater than 0.');
-            return;
-        }
-
-        betAmountElement.textContent = `$${betAmount.toFixed(2)}`;
-        placeBetButton.disabled = true;
-        cashOutButton.disabled = false;
-        intervalId = startGame();
-    } else {
-        // Handle game reset when "Place Bet" is clicked again
-        resetGame();
-    }
-});
-
-// Event listener for the "Cash Out" button
-cashOutButton.addEventListener('click', () => {
-    if (isRunning) {
-        const payout = betAmount * multiplier;
-        if (multiplier >= crashThreshold) {
-            endGame(true);
-        } else {
-            endGame(false, payout);
-        }
-        clearInterval(intervalId);
-    }
-});
-
-// Function to start the game
-function startGame() {
-    isRunning = true;
-    crashThreshold = getRandomCrashThreshold(4.00, 50.00);
-    updateRocketPosition(1);
-
-    return setInterval(() => {
-        updateGame();
-        if (multiplier < 50.00) { // End at 50.00
-            multiplier += speed; // Increment the multiplier by the current speed
-            multiplierElement.textContent = `${multiplier.toFixed(2)}x`;
-            // Gradually increase the speed as the game progresses
-            if (speed < 0.3) {
-                speed += 0.0003; // Adjust the increment value for speed
-            }
-            
-            // Check if the multiplier has reached or exceeded the crash threshold
-            if (multiplier >= crashThreshold) {
-                endGame(true);
-                clearInterval(intervalId);
-            }
-        } else {
-            clearInterval(intervalId);
-            endGame(true);
-        }
-    }, 100);
+    function generateCrashPoint() {
+        const min = 1; // Minimum crash point
+        const lambda = 0.5; // Adjust the lambda value to control the distribution
     
-}
-
-// Function to end the game and display the result
-function endGame(isAutomaticLoss = false, payout = 0) {
-    isRunning = false;
-    if (isAutomaticLoss) {
-        const loss = betAmount;
-        resultText.textContent = `You crashed and lost $${loss.toFixed(2)}`;
-    } else {
-        resultText.textContent = `You cashed out and won $${payout.toFixed(2)}`;
+        let crashPoint;
+    
+        // Generate a random value to occasionally set the crash point to 1.01
+        const randomValue = Math.random();
+    
+        if (randomValue < 0.1) { // 5% chance of getting 1.01
+            crashPoint = 1.00;
+        } else {
+            do {
+                const random = Math.random();
+    
+                // Using an exponential function to favor lower values
+                crashPoint = -Math.log(1 - random) / lambda;
+            } while (crashPoint <= 1.01); // Ensure crash point is higher than 1.01
+        }
+    
+        return crashPoint;
+            
     }
-    placeBetButton.disabled = false;
-    cashOutButton.disabled = true;
-}
 
+    function startGame() {
+        if (!gameStarted) {
+            gameStarted = true;
+            countdown.textContent = 'Game is running...';
+            gameContent.style.opacity = 0;
 
-function resetGame() {
-    isRunning = false;
-    betAmount = 0;
-    multiplier = 1.00;
-    speed = 0.002;
-    clearInterval(intervalId);
-    betAmountElement.textContent = '$0.00';
-    multiplierElement.textContent = '1.00x';
-    resultText.textContent = '';
-    placeBetButton.disabled = false;
-    cashOutButton.disabled = true;
-    rocket.style.bottom = `${initialRocketPosition}px`;
-    document.getElementById('graph').classList.remove('zoom');
-    setTimeout(() => {
-        document.getElementById('graph').classList.add('zoom');
-    }, 100);
-}
+            const crashPoint = generateCrashPoint();
+            console.log(crashPoint);
 
+            let increment = 0.001; // Starting with a slower increment
 
-/* JavaScript to update rocket position */
+            gameInterval = setInterval(function () {
+                currentMultiplier += increment;
+                multiplierValue.textContent = `x${currentMultiplier.toFixed(2)}`;
 
+                if (currentMultiplier >= crashPoint) {
+                    clearInterval(gameInterval);
+                    clearInterval(speedUpInterval); // Stop the speedup interval
+                    gameStarted = false;
+                    multiplierValue.textContent = `Crashed at x${currentMultiplier.toFixed(2)}`;
+                    historyData.push(currentMultiplier.toFixed(2)); // Add the crashed multiplier to history
+                    displayMultiplierHistory();
+    
+                    // Restoring button and input field states
+                    placeBetButton.disabled = false;
+                    placeBetButton.style.backgroundColor = '#4CAF50';
+                    betAmountInput.disabled = false;
+                    document.querySelector('label[for="bet-amount"]').textContent = `Bet Amount:`;
+                    cashOutButton.disabled = false;
+                    cashOutButton.style.backgroundColor = '#F44336';
+    
+                    // Handling game outcome display
+                    if (betAmount.toFixed(2) > 0.00 && gameContent.textContent === 'Game Content') {
+                        gameContent.textContent = 'You lost $' + betAmount.toFixed(2);
+                        gameContent.style.opacity = 1;
+                    }
+    
+                    // Clear bet amount and reset variables
+                    document.getElementById('bet-amount').value = '';
+                    betPlaced = false;
+                    countdownTime = 2;
+                    countdownIntervalFnc();
+    
+                    // Display multiplier history
+                    displayMultiplierHistory();
+                }
+                increment *= 1.02; // Multiply the increment for exponential growth
+            }, 100);
 
-// Function to update rocket position based on the multiplier
-function updateRocketPosition(multiplier) {
-    const maxHeight = 250; // Maximum height for the rocket to reach
-    const maxWidth = 800; // Maximum width for the rocket to reach
-    const newHeight = Math.min(maxHeight, maxHeight * (multiplier / 50));
-    const newLeft = maxWidth * (multiplier / 50); // Calculate the new left position
+            speedUpInterval = setInterval(function () {
+                currentMultiplier += 0.001; // Maintain a steady increase
+                multiplierValue.textContent = `x${currentMultiplier.toFixed(2)}`;
+            }, 100);
+        
+        }
+    }
 
-    // Apply the new positions to the rocket element with a smooth transition
-    rocket.style.bottom = `${newHeight}px`;
-    rocket.style.left = `${newLeft}px`;
-}
+    function countdownIntervalFnc() {
+        countdownInterval = setInterval(function () {
+            if (countdownTime <= 0) {
+                clearInterval(countdownInterval);
+                currentMultiplier = 1.00;
+                startGame();
+            } else {
+                countdown.textContent = `Game starts in: ${countdownTime} seconds`;
+                countdownTime--;
+            }
+        }, 1000);
+    }
 
-// Update the rocket position whenever the multiplier changes
-function updateGame() {
-    const multiplier = parseFloat(multiplierElement.textContent);
-    updateRocketPosition(multiplier);
-}
+    function displayMultiplierHistory() {
+        multiplierHistory.innerHTML = '';
 
-// Call updateGame whenever the multiplier changes (e.g., in your game logic)
-// Example:
-// multiplierElement.textContent = '2.50x'; // Update the multiplier
-// updateGame(); // Update the rocket position
+        const lastTenMultipliers = historyData.slice(-10).reverse(); // Retrieve the last ten multipliers and reverse the order
 
+        lastTenMultipliers.forEach(function (multiplier, index) {
+            const li = document.createElement('li');
+            li.textContent = `x${multiplier}`;
+            if (index === 0) {
+                li.textContent += ' (Latest)';
+            }
+            multiplierHistory.appendChild(li);
+        });
+    }
 
-// Helper function to get a random crash threshold within a range
-function getRandomCrashThreshold(min, max) {
-    return Math.random() * (max - min) + min;
-}
+    // Event listener for preset buttons
+    presetButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            if (!gameStarted && !betPlaced) {
+                const value = button.textContent;
+                betAmountInput.value = value;
+            }
+        });
+    });
 
-// Helper function to get a random increment for the multiplier
-function getRandomIncrement() {
-    return Math.random() * 0.1 + 0.05; // Adjust the range as needed
-}
+    // Event listener for placing a bet
+    placeBetButton.addEventListener('click', function () {
+        if (!gameStarted) {
+            placeBetButton.disabled = false;
+            betAmount = parseFloat(betAmountInput.value);
+            if (isNaN(betAmount) || betAmount <= 0) {
+            } else {
+                placeBetButton.disabled = true;
+                placeBetButton.style.backgroundColor = 'grey';
+                betAmountInput.disabled = true;
+                document.querySelector('label[for="bet-amount"]').textContent = `Bet Amount: $${betAmount.toFixed(2)}`;
+                betPlaced = true;
+            }
+        }
+    });
+
+    // Event listener for cashing out
+    cashOutButton.addEventListener('click', function () {
+        if (gameStarted && betPlaced) {
+            cashOutButton.disabled = true;
+            cashOutButton.style.backgroundColor = 'grey';
+            gameContent.style.opacity = 1;
+            gameContent.textContent = 'You cashed out at x' + currentMultiplier.toFixed(2) + ' and won $' + (betAmount * currentMultiplier).toFixed(2);
+        }
+    });
+
+    // Event listener for bet amount input
+    betAmountInput.addEventListener('input', function () {
+        betAmountInput.value = betAmountInput.value.replace(/[^0-9.]/g, '');
+    });
+});
