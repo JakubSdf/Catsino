fd9d2c1f4285def12a1ea93882a0c9bed4b2479f
Jimmy <sikulajakub2005@gmail.com>
2023-12-22 18:25:10 +0100

    added templates, base ready

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
-# importing Flask and other modules
-import os
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('main.html')
 
-@app.route('/ruleta')
+@app.route('/roulette')
 def ruleta():
-    return render_template('ruleta.html')
+    return render_template('roulette.html')
 
 
 @app.route('/crash')
@@ -22,9 +21,13 @@ def blackjack():
     return render_template('blackjack.html')
 
 
-@app.route('/maty')
+@app.route('/slots')
 def maty():
-    return render_template('maty.html')
+    return render_template('slots.html')
+
+@app.route('/games')
+def games():
+    return render_template('games.html')
 
 
 if __name__ == "__main__":
